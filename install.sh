b6dcce0ef9adc08cc7c5776a224061de6d693681
Peter Hickman <peterhickman386@googlemail.com>
2018-10-19 22:25:33 +0100

    an install script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+WHITELIST='/etc/ban_hammer/whitelist'
+BLACKLIST='/etc/ban_hammer/blacklist'
+
+create_blank() {
+  if [ -r $1 ]; then
+    echo "File $1 already exists"
+  else
+    echo "Creating blank file: $1"
+    touch $1
+  fi
+}
+
+if ! [ $(id -u) = 0 ]; then
+  echo 'You must be root to do this.' 1>&2
+  exit 1
+fi
+
+echo 'Installing Ban Hammer for ufw to /usr/local/sbin/'
+install -g root -o root -m 0700 bh /usr/local/sbin/
+
+if [ -d '/etc/ban_hammer' ]; then
+  echo 'Directory /etc/ban_hammer already exists'
+else
+  echo 'Creating directory in /etc'
+  mkdir -p /etc/ban_hammer
+fi	
+
+create_blank $WHITELIST
+create_blank $BLACKLIST
+
+echo "Make sure that you populate $WHITELIST"
+echo "with a list of the addresses you never"
+echo "want to be banned"
