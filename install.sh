32be917d13759f335769304da498d25158ee8af0
Peter Hickman <peterhickman386@googlemail.com>
2018-11-06 21:43:36 +0000

    still learning

--- install.sh
+++ install.sh
@@ -5,6 +5,13 @@ if ! [ "$(id -u)" = 0 ]; then
   exit 1
 fi
 
+##
+# When installing we would install as root, however on
+# BSD based systems (such as OSX) this is not a valid
+# group / user for install so we use the numberic value
+##
+ROOT="0"
+
 WHITELIST='/etc/ban_hammer/whitelist'
 BLACKLIST='/etc/ban_hammer/blacklist'
 
@@ -19,11 +26,11 @@ create_blank() {
 
 install_cron() {
   echo "Installing cron into $1"
-  install -g root -o root -m 0755 "$1" "/etc/$1/banhammer"
+  install -g $ROOT -o $ROOT -m 0755 "$1" "/etc/$1/banhammer"
 }
 
 echo 'Installing Ban Hammer for ufw to /usr/local/sbin/'
-install -g root -o root -m 0700 bh /usr/local/sbin/
+install -g $ROOT -o $ROOT -m 0700 bh /usr/local/sbin/
 
 if [ -d '/etc/ban_hammer' ]; then
   echo 'Directory /etc/ban_hammer already exists'
@@ -40,7 +47,7 @@ install_cron cron.hourly
 install_cron cron.weekly
 
 echo "Install the logrotate file"
-install -g root -o root -m 0644 logrotate /etc/logrotate.d/ban_hammer
+install -g $ROOT -o $ROOT -m 0644 logrotate /etc/logrotate.d/ban_hammer
 
 echo
 echo "Make sure that you populate $WHITELIST"
